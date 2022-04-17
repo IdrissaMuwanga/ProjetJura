8b7bfb3f9e3dddf3a86de286d5995d2d16019f12
Idrissa Muwanga <80053968+IdrissaMuwanga@users.noreply.github.com>
2022-04-17 12:48:29 +0200

    Delete app directory

--- app/Helpers/From_Helper.php
+++
@@ -1,8 +0,0 @@
-<?php function displayError($validation, $field)
-{
-    if ($validation->hasError($field)) {
-        return $validation->getError($field);
-    } else {
-        return false;
-    }
-}
